7756c40e21fffe433e499a72e978969622c246d8
Serpant1ne <gartsmankonstantin@gmail.com>
2024-05-02 15:54:09 +0200

    implemented localStorage with saving state

--- dist/script.js
+++ dist/script.js
@@ -1,31 +1,32 @@
-function updateTask(task) {
-    task.done = !task.done;
+//TODO add read and write to localstorage
+function WriteToLocalstorage(data, name) {
+    localStorage.setItem(name, JSON.stringify(data));
+}
+function GetFromLocalStorage(name) {
+    return JSON.parse(localStorage.getItem(name));
+}
+function updateToDoStatus(id) {
+    const taskList = GetFromLocalStorage("todoList");
+    taskList.forEach((task) => {
+        if (task.id === id) {
+            task.done = !task.done;
+        }
+    });
+    WriteToLocalstorage(taskList, "todoList");
 }
 //function that draws the list of tasks in HTML
-function renderTasks(taskList, parent) {
+function renderTasks(parent) {
     //clearing parent list
     parent.innerHTML = "";
+    const taskList = GetFromLocalStorage("todoList");
     taskList.forEach(task => {
-        //creating and defining elements
-        let wrapper = document.createElement("div");
-        wrapper.classList.add("task");
-        let nameElem = document.createElement("h3");
-        nameElem.classList.add("taskName");
-        nameElem.innerHTML = task.name;
-        let checkboxElem = document.createElement("input");
-        checkboxElem.type = "checkbox";
-        checkboxElem.classList.add("checkbox");
-        checkboxElem.checked = task.done;
-        if (checkboxElem) {
-            wrapper.classList.add("done");
-        }
-        wrapper.append(nameElem, checkboxElem);
-        parent.append(wrapper);
+        parent.append(createTaskElement(task));
     });
 }
 //function that create task from the input with default not done
 function createTask(taskName, status) {
     return {
+        id: Date.now(),
         name: taskName,
         done: status === undefined ? false : status,
     };
@@ -35,20 +36,27 @@ function createTaskElement(task) {
     //creating parent element
     const parent = document.createElement("li");
     parent.classList.add("task");
+    let name = document.createElement("h3");
+    name.classList.add("taskName");
+    name.innerHTML = task.name;
     //add checkbox and it's functionality
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
     checkbox.checked = task.done;
-    checkbox.addEventListener('change', (e) => {
-        task.done = !task.done;
+    checkbox.addEventListener('change', () => {
+        updateToDoStatus(task.id);
     });
+    parent.append(name, checkbox);
     return parent;
 }
 //initialization function. It gets all the elements from index.html, adds functionality to form, 
 function initialize() {
-    // creating variables
-    let taskList = [];
+    //get data from localStorage
+    let taskList = GetFromLocalStorage("todoList");
     let list = document.getElementById("list");
+    //Render task from localstorage
+    renderTasks(list);
     // get form for task creation
     let form = document.getElementById("form");
     let taskNameInput = document.getElementById("taskNameInput");
@@ -56,10 +64,10 @@ function initialize() {
         e.preventDefault();
         // submit in list only when name is not empty
         if (taskNameInput.value != "") {
-            console.log(taskNameInput.value);
             taskList.push(createTask(taskNameInput.value));
             taskNameInput.value = "";
-            renderTasks(taskList, list);
+            WriteToLocalstorage(taskList, "todoList");
+            renderTasks(list);
         }
     });
 }
--- src/script.ts
+++ src/script.ts
@@ -1,41 +1,44 @@
+//TODO add tasks delete
+
 type Task = {
+    id: number,
     name: string,
     done: boolean
 }
 
-function updateTask(task: Task): void {
-    task.done = !task.done
+function WriteToLocalstorage(data: Task[], name: string): void{
+    localStorage.setItem(name, JSON.stringify(data))
+}
+
+function GetFromLocalStorage(name: string): Task[] {
+    return JSON.parse(localStorage.getItem(name))
+}
+
+function updateToDoStatus(id: number): void{
+    const taskList = GetFromLocalStorage("todoList")
+    taskList.forEach((task) => {
+        if(task.id === id){
+            task.done = !task.done
+        }
+    })
+    WriteToLocalstorage(taskList, "todoList")
 }
 
 //function that draws the list of tasks in HTML
-function renderTasks(taskList: Task[], parent: HTMLElement): void {
+function renderTasks(parent: HTMLElement): void {
     //clearing parent list
     parent.innerHTML = ""
+
+    const taskList = GetFromLocalStorage("todoList")
     taskList.forEach(task => {
-        //creating and defining elements
-        let wrapper = document.createElement("div")
-        wrapper.classList.add("task")
-
-        let nameElem = document.createElement("h3")
-        nameElem.classList.add("taskName")
-        nameElem.innerHTML = task.name
-
-        let checkboxElem = document.createElement("input")
-        checkboxElem.type = "checkbox"
-        checkboxElem.classList.add("checkbox")
-        checkboxElem.checked = task.done
-        if(checkboxElem){
-            wrapper.classList.add("done")
-        }
-        
-        wrapper.append(nameElem, checkboxElem)
-        parent.append(wrapper)
+        parent.append(createTaskElement(task))
     })
 }
 
 //function that create task from the input with default not done
 function createTask(taskName: string, status?: boolean): Task | null {
     return {
+        id: Date.now(),
         name: taskName,
         done: status === undefined ? false : status,
     }
@@ -47,38 +50,43 @@ function createTaskElement(task: Task): HTMLElement | null {
     const parent = document.createElement("li")
     parent.classList.add("task")
 
-
+    let name = document.createElement("h3")
+    name.classList.add("taskName")
+    name.innerHTML = task.name
 
     //add checkbox and it's functionality
     let checkbox = document.createElement("input")
     checkbox.type = "checkbox"
+    checkbox.classList.add("checkbox")
     checkbox.checked = task.done
-    checkbox.addEventListener('change', (e) => {
-        task.done = !task.done
+    
+    checkbox.addEventListener('change', () => {
+        updateToDoStatus(task.id)
     })
+    parent.append(name, checkbox)
 
     return parent
 }
 
 //initialization function. It gets all the elements from index.html, adds functionality to form, 
 function initialize() {
-    // creating variables
-    let taskList: Task[] = []
+    //get data from localStorage
+    let taskList: Task[] = GetFromLocalStorage("todoList")
 
     let list = document.getElementById("list")
-
+    //Render task from localstorage
+    renderTasks(list)
     // get form for task creation
     let form = document.getElementById("form")
     let taskNameInput = document.getElementById("taskNameInput") as HTMLInputElement
     form.addEventListener("submit", (e) => {
         e.preventDefault()
-        
         // submit in list only when name is not empty
         if(taskNameInput.value != ""){
-            console.log(taskNameInput.value)
             taskList.push(createTask(taskNameInput.value))
             taskNameInput.value = ""
-            renderTasks(taskList, list)
+            WriteToLocalstorage(taskList, "todoList")
+            renderTasks(list)
         }
         
     })
